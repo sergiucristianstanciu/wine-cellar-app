7d9e43b441b93e6344b1d66ab52513858c28728d
sergiucstanciu <sergiucristianstanciu@gmail.com>
2024-05-20 17:37:29 +0300

    implemented basic layout

--- wine-cellar-app/src/app/app.component.ts
+++ wine-cellar-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonAccordion, IonAccordionGroup, IonApp, IonContent, IonItem, IonMenu, IonRouterOutlet, IonSplitPane } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonMenu, IonContent, IonSplitPane, IonAccordionGroup, IonAccordion, IonItem],
 })
 export class AppComponent {
   constructor() {}
